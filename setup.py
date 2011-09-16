c48bdbb68491d5ad180de0b5b0aad4715dde1e49
Eric St-Jean <esj@wwd.ca>
2011-09-16 15:11:23 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+# Copyright (c) Ecometrica. All rights reserved.
+# Distributed under the BSD license. See LICENSE for details.
+
+from setuptools import setup, find_packages
+setup(
+    name = "django-vcerver",
+    version = "0.0.1",
+    packages = find_packages(),
+)
